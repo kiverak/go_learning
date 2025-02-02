c7a833d5abac44b93e2e284bdd5ae79f4669befa
Виталий Бородин <kiverak@gmail.com>
2025-02-03 00:19:39 +0500

    start types

---
+++ 3.2-types/1-types.go
@@ -0,0 +1,17 @@
+package string
+
+import (
+	"fmt"
+)
+
+func DoTypes1() {
+	var num int64
+	fmt.Scan(&num)
+
+	convert(num)
+	fmt.Println(num)
+}
+
+func convert(x int64) uint16 {
+	return uint16(x)
+}
--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	//. "go_learning/2.5-strings"
 	//. "go_learning/2.6-errors"
 	//. "go_learning/2.7-exercises"
-	. "go_learning/3.1-maps"
+	//. "go_learning/3.1-maps"
+	. "go_learning/3.2-types"
 )
 
 func main() {
 	s := "gopher"
 	fmt.Printf("Hello and welcome, %s!\n", s)
 
-	DoMap2()
+	DoTypes1()
 }
